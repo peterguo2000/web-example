f9eac7dac487fefeddd56e5c8dffc61a41065981
peterguo2000 <peter.k.guo@gmail.com>
2019-04-01 22:35:41 +1100

    Delete data.sql

--- myapp5/dbwatcher-api/bin/main/data.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS demo (id INT NOT NULL, description VARCHAR(50) NOT NULL);
-INSERT INTO demo (id, description) VALUES(1, 'Add a third todo');
-INSERT INTO demo (id, description) VALUES(2, 'Add a third todo');
